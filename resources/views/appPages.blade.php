c52cfddbd18a73d5c38ac3caf800f29601b067bd
Norayr Baghdasarov <noro9424@gmail.com>
2016-07-02 15:55:57 +0400

    update 02-07-2016

--- resources/views/appPages.blade.php
+++ resources/views/appPages.blade.php
@@ -9,10 +9,30 @@
 </head>
 <body>
 
-
-
-
-
+    <section class="headerMenu">
+        <nav class="">
+            <div class="container-fluid">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 static-header-logo">
+                    <div class="site-branding">
+                        <h1 class="site-title image-logo">
+                            <a class="" href="/">
+                                <img src="/images/resources/logo.png" alt="FINE ART">
+                            </a>
+                        </h1>
+                        <div class="site-description">gallery</div>
+                    </div>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Домашняя</a></li>
+                    <li><a href="#">О наc</a></li>
+                    <li><a href="#">голлерея</a></li>
+                    <li><a href="#">блог</a></li>
+                    <li><a href="#">контакт</a></li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+    @yield("slider")
 
     {{--SCRIPT--}}
     <script src="/js/jquery/test/data/jquery-1.9.1.js" ></script>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,2 +1,22 @@
 @extends('appPages')
 
+@section('slider')
+    <section class="slider">
+        <ul class="rslides">
+            <li>
+                <img src="images/resources/logBackground.jpg" alt="">
+            </li>
+            <li>
+                <img src="images/resources/slider_03.jpg" alt="">
+            </li>
+            <li>
+                <img src="images/resources/slider_02.jpg" alt="">
+            </li>
+        </ul>
+    </section>
+@stop
+@section("script")
+    <script src="js/ResponsiveSlides/responsiveslides.min.js"></script>
+    <script src="js/pageScript.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+@stop
